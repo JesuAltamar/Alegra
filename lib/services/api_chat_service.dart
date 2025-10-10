3b0ae6f0095aeded2b3756365c4229f6de1d0f3e
JesuAltamar <jesus-altamar.25@hotmail.com>
2025-10-10 00:20:22 -0500

    Connect to Railway backend

--- lib/services/api_chat_service.dart
+++ lib/services/api_chat_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiChatService {
-  static const String baseUrl = 'http://backendproyecto-producción-4a8d.up.railway.app:5000';
+  static const String baseUrl = 'http://backendproyecto-producción-4a8d.up.railway.app';
   
   /// Enviar mensaje al chat
   Future<Map<String, dynamic>> sendMessage({
