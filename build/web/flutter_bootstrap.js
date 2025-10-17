743d4bd9462b9129b37fcd23fb48e17efeb763e5
JesuAltamar <jesus-altamar.25@hotmail.com>
2025-10-16 21:27:23 -0500

    deploy: responsive calendar

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3296054314"
+    serviceWorkerVersion: "1667915616"
   }
 });
