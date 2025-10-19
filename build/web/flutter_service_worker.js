f121c297c5d0facb999fd8a086a287a4ab00bbff
JesuAltamar <jesus-altamar.25@hotmail.com>
2025-10-19 17:46:53 -0500

    borrar cache

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8e45b2bd3662b6de641411a95721dc93
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b7158868f8e085e495917be73746eb4b",
+"flutter_bootstrap.js": "735a682448ee5e7288e8a2a90e37fdfb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
